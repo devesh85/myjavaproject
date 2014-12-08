096144db7435304154905999161142a5dd29f3fd
devesh <deveshk@gmail.com>
2014-12-07 22:51:47 -0500

    Starting with start, have an interface collection in the begining

---
+++ src/main/java/my/collections/MyCollection.java
@@ -0,0 +1,13 @@
+package my.collections;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+/**
+ * Created by devesh on 12/7/14.
+ */
+public interface MyCollection<T> extends Iterable<T> {
+
+}
---
+++ src/test/java/my/lists/TestMyArrayList.java
@@ -0,0 +1,25 @@
+package my.lists;
+
+import my.collections.MyCollection;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+/**
+ * Created by devesh on 12/7/14.
+ */
+public class TestMyArrayList {
+    public MyCollection<String> myCollection;
+
+    @Before
+    public void setUp() throws Exception{
+
+    }
+
+    @Test
+    public void testListSize() throws Exception{
+//        assert(myCollection)
+
+    }
+}
